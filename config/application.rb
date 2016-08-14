4aeb65ca9614e7ff3f94d5e6a72c4c37d5781448
RubenSloote <rubensloote@gmail.com>
2016-08-14 20:35:39 +0200

    seed plus

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Songdb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     console do
-      ActiveRecord::base.connection
+      ActiveRecord::Base.connection
     end
   end
 end
