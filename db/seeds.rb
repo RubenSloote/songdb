c1fd1751850b84195734f236dac2efe1b977b8b1
RubenSloote <rubensloote@gmail.com>
2016-08-14 20:21:10 +0200

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.create( { song: "The Virus" } )
+Song.create( { song: "Lights Please" } )
+Song.create( { song: "Wanna Be Hard" } )
+Song.create( { song: "No. 99" } )
+Song.create( { song: "Song Cry" } )
+
+Artist.create( { name: "Tech N9ne" } )
+Artist.create( { name: "J. Cole" } )
+Artist.create( { name: "Kendrick Lamar" } )
+Artist.create( { name: "Joey Bada$$" } )
+Artist.create( { name: "Jay-Z" } )
