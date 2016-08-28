0a7c1b2a8b84acc4fedb3ee45ca7a90320bbf988
RubenSloote <rubensloote@gmail.com>
2016-08-28 19:24:05 +0200

    add cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'djdcx8hbj'
+  config.api_key = '851819848286546'
+  config.api_secret = 'h9rsEMvYiJ6scANinwcls0ADQ5A'
+  config.cdn_subdomain = true
+end
