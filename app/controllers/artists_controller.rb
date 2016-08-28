cec14c14fcd37be363579a9f31fc5c8e1e61c21e
RubenSloote <rubensloote@gmail.com>
2016-08-28 23:37:06 +0200

    almost working show page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,39 @@
 class ArtistsController < ApplicationController
 
+    before_action :set_song, only: [:destroy]
+
   def index
     @artists = Artist.order("#{sort_column} #{sort_direction}")
+    @songs = Song.all
   end
 
   def show
     @artists = Artist.find(params[:id])
+    @songs = Song.all
   end
 
+  def new
+  @artist = Artist.new
+  end
 
+  def create
+  @artist = Artist.new(artist_params)
+    if @artist.save
+    redirect_to root_path
+    else
+    render 'new'
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+  end
+
+  def destroy_all
+    @artist = Artist.find(params[:id])
+    Song.destroy_all(artist_id: @artist)
+  end
 
   private
   def sortable_columns
@@ -21,6 +46,10 @@ class ArtistsController < ApplicationController
 
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 Song.delete_all
 Artist.delete_all
 
-tech = Artist.create( name: "Tech N9ne" )
-cole = Artist.create( name: "J. Cole" )
-kendrick = Artist.create( name: "Kendrick Lamar" )
-joey = Artist.create( name: "Joey Bada$$" )
-jay = Artist.create( name: "Jay-Z" )
+tech = Artist.create( name: "Tech N9ne", remote_image_url: "https://cdn1-strangemusicinc.netdna-ssl.com/wp-content/uploads/2011/01/Tech-N9ne-Sickology-101.jpg" )
+cole = Artist.create( name: "J. Cole", remote_image_url: "http://www.puna.nl/wp-content/uploads/2015/12/J.-Cole-Homecoming-LYFSTYL.jpg" )
+kendrick = Artist.create( name: "Kendrick Lamar", remote_image_url: "https://consequenceofsound.files.wordpress.com/2013/08/kendrick-lamar-king.png" )
+joey = Artist.create( name: "Joey Bada$$", remote_image_url: "http://photon.hypb.st/hypetrak.com/images/2015/06/joey-bada-claims-to-be-the-1-independant-hip-hop-artist-in-the-world.jpg?resize=617,411" )
+jay = Artist.create( name: "Jay-Z", remote_image_url: "https://images.genius.com/0d53c56a247ef39e4106718deb95f347.1000x500x1.jpg" )
 
-Song.create( song: "The Virus", artist: tech )
-Song.create( song: "Lights Please", artist: cole )
-Song.create( song: "Wanna Be Hard", artist: kendrick )
-Song.create( song: "No. 99", artist: joey )
-Song.create( song: "Song Cry", artist: jay )
+Song.create({ song: "The Virus", artist: tech })
+Song.create({ song: "Lights Please", artist: cole })
+Song.create({ song: "Wanna Be Hard", artist: kendrick })
+Song.create({ song: "No. 99", artist: joey })
+Song.create({ song: "Song Cry", artist: jay })
