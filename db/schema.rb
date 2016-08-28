2b5af526b6f706747658be1e904f264ea2e8dc7a
RubenSloote <rubensloote@gmail.com>
2016-08-28 21:58:13 +0200

    new seeds and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814184047) do
+ActiveRecord::Schema.define(version: 20160828191849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20160814184047) do
     t.string   "song"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
   add_foreign_key "artists", "songs"
+  add_foreign_key "songs", "artists"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-Artist.delete_all
 Song.delete_all
+Artist.delete_all
 
-virus = Song.create(  song: "The Virus"  )
-lights = Song.create(  song: "Lights Please"  )
-wanna = Song.create(  song: "Wanna Be Hard"  )
-no = Song.create(  song: "No. 99"  )
-cry = Song.create(  song: "Song Cry"  )
+tech = Artist.create( name: "Tech N9ne" )
+cole = Artist.create( name: "J. Cole" )
+kendrick = Artist.create( name: "Kendrick Lamar" )
+joey = Artist.create( name: "Joey Bada$$" )
+jay = Artist.create( name: "Jay-Z" )
 
-Artist.create(  name: "Tech N9ne", song_id: virus  )
-Artist.create(  name: "J. Cole", song_id: lights  )
-Artist.create(  name: "Kendrick Lamar", song_id: wanna  )
-Artist.create(  name: "Joey Bada$$", song_id: no  )
-Artist.create(  name: "Jay-Z", song_id: cry  )
+Song.create( song: "The Virus", artist: tech )
+Song.create( song: "Lights Please", artist: cole )
+Song.create( song: "Wanna Be Hard", artist: kendrick )
+Song.create( song: "No. 99", artist: joey )
+Song.create( song: "Song Cry", artist: jay )
